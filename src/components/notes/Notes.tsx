5484f5605f0790d3699f07e48dd3fd3ff0bfc777
Karol Wolski <karolwolski04@gmail.com>
2022-10-19 16:01:12 +0200

    refactor (Notes): Add map loop for notes array (#28)

--- src/components/notes/Notes.tsx
+++ src/components/notes/Notes.tsx
@@ -1,48 +1,82 @@
-import Note from '../note/Note'
+import { useState } from 'react'
+import Note, { INote } from '../note/Note'
 
 const Notes = () => {
+  const [notes, setNotes] = useState<INote[]>([
+    {
+      id: '0001',
+      title: 'Note 1',
+      desc: 'This is note number 1',
+    },
+    {
+      id: '0002',
+      title: 'Note 2',
+      desc: 'This is note number 2',
+    },
+    {
+      id: '0003',
+      title: 'Note 3',
+      desc: 'This is note number 3',
+    },
+    {
+      id: '0004',
+      title: 'Note 4',
+      desc: 'This is note number 4',
+    },
+    {
+      id: '0005',
+      title: 'Note 5',
+      desc: 'This is note number 5',
+    },
+    {
+      id: '0006',
+      title: 'Note 6',
+      desc: 'This is note number 6',
+    },
+    {
+      id: '0007',
+      title: 'Note 7',
+      desc: 'This is note number 7',
+    },
+    {
+      id: '0008',
+      title: 'Note 8',
+      desc: 'This is note number 8',
+    },
+  ])
+
   return (
     <div className='container'>
-      <div className='row g-3'>
-        <div className='col-12 col-sm-6 col-md-4 col-xl-3'>
-          <Note id='note0001' title='Note 01' desc='This is note' />
-        </div>
-        <div className='col-12 col-sm-6 col-md-4 col-xl-3'>
-          <Note
-            id='note0002'
-            title='Note 02'
-            desc='This is note'
-            img='https://pixabay.com/get/g68e094ec2e8cc52ca41892dc70806fd1432bfb3f56aba70c38a43a3c873ba0b30a7a4d350552d4f3280b6d476a8ef291_1280.jpg'
-          />
-        </div>
-        <div className='col-12 col-sm-6 col-md-4 col-xl-3'>
-          <Note
-            id='note0003'
-            title='Note 03'
-            desc='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor, esse temporibus sequi molestias minima minus asperiores ab molestiae adipisci excepturi aspernatur expedita. Cum incidunt libero asperiores corrupti vero nesciunt, id accusamus eos minima, quaerat error, aut officiis adipisci consectetur facilis.'
-          />
-        </div>
-        <div className='col-12 col-sm-6 col-md-4 col-xl-3'>
-          <Note id='note0004' title='Note 04' desc='This is note' />
-        </div>
-        <div className='col-12 col-sm-6 col-md-4 col-xl-3'>
-          <Note id='note0005' title='Note 05' desc='This is note' />
-        </div>
-        <div className='col-12 col-sm-6 col-md-4 col-xl-3'>
-          <Note id='note0006' title='Note 06' desc='This is note' />
-        </div>
-        <div className='col-12 col-sm-6 col-md-4 col-xl-3'>
-          <Note id='note0007' title='Note 07' desc='This is note' />
-        </div>
-        <div className='col-12 col-sm-6 col-md-4 col-xl-3'>
-          <Note id='note0008' title='Note 08' desc='This is note' />
-        </div>
-        <div className='col-12 col-sm-6 col-md-4 col-xl-3'>
-          <Note id='note0009' title='Note 09' desc='This is note' />
-        </div>
-      </div>
+      <>
+        {notes.length > 0 ? (
+          <div className='row g-3'>
+            {notes.map(note => (
+              <div key={note.id} className='col-12 col-sm-6 col-md-4 col-xl-3'>
+                <Note id={note.id} title={note.title} desc={note.desc} />
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className='row'>
+            <p className='fs-4 text-center'>You do not have any notes yet.</p>
+            <p className='text-center'>
+              Click <span className='fw-semibold'>Add note</span> button and add your first note.
+            </p>
+          </div>
+        )}
+      </>
     </div>
   )
 }
 
 export default Notes
+
+// {
+//               notes.map(note => {
+//                   return (
+//                     <div key={note.id} className='col-12 col-sm-6 col-md-4 col-xl-3'>
+//                       <Note id={note.id} title={note.title} desc={note.desc} />
+//                     </div>
+//                   )
+
+//               }
