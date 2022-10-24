e1567233b18cf62bf37e0bf574d205da62ae2f91
Karol Wolski <karolwolski04@gmail.com>
2022-10-24 10:45:18 +0200

    fix: Close Modal after clicking the close icon (#36)

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -13,7 +13,13 @@ const Modal = ({ title, desc, btnName, handleBtnEvent, handleClose }: IModal) =>
         <div className='modal-content'>
           <div className='modal-header'>
             <h5 className='modal-title'>{title}</h5>
-            <button type='button' className='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
+            <button
+              type='button'
+              className='btn-close'
+              data-bs-dismiss='modal'
+              aria-label='Close'
+              onClick={handleClose}
+            ></button>
           </div>
           <div className='modal-body'>
             <p>{desc}</p>
