81c8319a0a415d9882e603b41e83878485a5228c
Karol Wolski <karolwolski04@gmail.com>
2022-10-27 20:22:39 +0200

    refactor(Note): set the correct button colors

--- src/components/note/Note.tsx
+++ src/components/note/Note.tsx
@@ -54,20 +54,15 @@ const Note = ({ note, handleRemoveNote, handleDuplicateNote, handleEditNote }: I
           </div>
           <div className='d-grid gap-2 d-md-flex justify-content-md-between mt-2'>
             <div className='gap-2 d-flex justify-content-start'>
-              <button
-                type='button'
-                className='btn btn-outline-danger btn-sm'
-                onClick={openRemoveModal}
-                title='Remove note'
-              >
+              <button type='button' className='btn btn-danger btn-sm' onClick={openRemoveModal} title='Remove note'>
                 <i className='bi bi-trash'></i>
               </button>
-              <button type='button' className='btn btn-outline-danger btn-sm' onClick={openEditModal} title='Edit note'>
+              <button type='button' className='btn btn-warning btn-sm' onClick={openEditModal} title='Edit note'>
                 <i className='bi bi-pencil'></i>
               </button>
               <button
                 type='button'
-                className='btn btn-outline-danger btn-sm'
+                className='btn btn-dark btn-sm'
                 onClick={() => handleDuplicateNote(id)}
                 title='Duplicate note'
               >
