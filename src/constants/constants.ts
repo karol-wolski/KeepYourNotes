516449d0fd13e8d322c16b8bf42f6ed119757bb1
Karol Wolski <karolwolski04@gmail.com>
2022-11-09 12:02:32 +0100

    feat: Set background color to the note (#48)

---
+++ src/constants/constants.ts
@@ -0,0 +1,58 @@
+const BG_COLORS = [
+  {
+    id: 1,
+    name: 'black',
+    bgColor: '#191919',
+    color: '#FFF',
+  },
+  {
+    id: 2,
+    name: 'green',
+    bgColor: '#4cfcaa',
+    color: '#000',
+  },
+  {
+    id: 3,
+    name: 'yellow',
+    bgColor: '#fce979',
+    color: '#000',
+  },
+  {
+    id: 4,
+    name: 'red',
+    bgColor: '#880808',
+    color: '#fff',
+  },
+  {
+    id: 5,
+    name: 'orange',
+    bgColor: '#fa9052',
+    color: '#000',
+  },
+  {
+    id: 6,
+    name: 'blue',
+    bgColor: '#A2D2FF',
+    color: '#000',
+  },
+  {
+    id: 7,
+    name: 'navy-blue',
+    bgColor: '#021e4a',
+    color: '#fff',
+  },
+  {
+    id: 8,
+    name: 'pink',
+    bgColor: '#fc72f8',
+    color: '#000',
+  },
+  {
+    id: 9,
+    name: 'grey',
+    bgColor: '#424242',
+    color: '#fff',
+  },
+]
+
+export { BG_COLORS }
