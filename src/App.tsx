c3b82d1b8c9f2d34165e4525f0189c71ce9c247a
Karol Wolski <karolwolski04@gmail.com>
2022-11-13 19:47:06 +0100

    feat: Login Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import LoginPage from './pages/Login'
 import NotesPage from './pages/Notes'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<NotesPage />} />
+        <Route path='/login' element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   )
