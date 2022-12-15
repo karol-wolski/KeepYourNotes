d7c7b5244d8a9b336677e49f2a842efdedc6f630
Karol Wolski <karolwolski04@gmail.com>
2022-12-15 22:16:14 +0100

    feat: Integration add note with BE (#79)

--- src/helpers/localStorage.ts
+++ src/helpers/localStorage.ts
@@ -1,5 +1,4 @@
 export const getFromLocalStorage = (key: string) => localStorage.getItem(key)
-export const addToLocalStorage = (key: string, value: number | string) =>
-  localStorage.setItem(key, JSON.stringify(value))
+export const addToLocalStorage = (key: string, value: string) => localStorage.setItem(key, value)
 export const removeFromLocalStorage = (key: string) => localStorage.removeItem(key)
 export const clearLocalStorage = () => localStorage.clear()
