0a93c6b0009031ccf5c48585ba78634800712a6c
Karol Wolski <karolwolski04@gmail.com>
2022-12-26 17:06:18 +0100

    refactor: Add category(#96)

--- src/components/add-category/AddCategory.tsx
+++ src/components/add-category/AddCategory.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-import uuid from 'react-uuid'
+import LabelInput from '../labelInput/LabelInput'
+import Modal from '../modal/Modal'
 
 export type Category = {
   _id: string
@@ -13,13 +14,13 @@ interface IAddCategory {
 
 const AddCategory = ({ handleClose, handleSaveCategory }: IAddCategory) => {
   const [category, setCategory] = useState<Category>({
-    _id: uuid(),
+    _id: '',
     name: '',
   })
 
   const isVisibleSendButton = !!category.name.length
 
-  const createNote = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const createCategory = (event: React.ChangeEvent<HTMLInputElement>) => {
     setCategory({
       ...category,
       [event.target.name]: event.target.value,
@@ -27,50 +28,24 @@ const AddCategory = ({ handleClose, handleSaveCategory }: IAddCategory) => {
   }
 
   return (
-    <div className='modal fade show' tabIndex={-1} style={{ display: 'block', background: 'rgba(0, 0, 0, 0.5)' }}>
-      <div className='modal-dialog'>
-        <div className='modal-content'>
-          <div className='modal-header'>
-            <h5 className='modal-title'>Add note</h5>
-            <button
-              type='button'
-              className='btn-close'
-              data-bs-dismiss='modal'
-              aria-label='Close'
-              onClick={handleClose}
-            ></button>
-          </div>
-          <div className='modal-body'>
-            <div className='mb-3'>
-              <label htmlFor='category-name' className='form-label'>
-                Title
-              </label>
-              <input
-                type='text'
-                className='form-control'
-                id='category-name'
-                placeholder='Category name'
-                name='name'
-                onChange={createNote}
-              />
-            </div>
-          </div>
-          <div className='modal-footer'>
-            <button type='button' className='btn btn-secondary' data-bs-dismiss='modal' onClick={handleClose}>
-              Close
-            </button>
-            <button
-              type='button'
-              className='btn btn-primary'
-              onClick={() => handleSaveCategory(category)}
-              disabled={!isVisibleSendButton}
-            >
-              Add Category
-            </button>
-          </div>
-        </div>
+    <Modal
+      title='Add category'
+      handleClose={handleClose}
+      btnName='Save'
+      handleBtnEvent={() => handleSaveCategory(category)}
+      isDisabledBtn={!isVisibleSendButton}
+    >
+      <div className='mb-3'>
+        <LabelInput
+          labelText='Category'
+          id='name'
+          onChange={createCategory}
+          type='text'
+          placeholder='Category name'
+          isLabelVisible={false}
+        />
       </div>
-    </div>
+    </Modal>
   )
 }
 
