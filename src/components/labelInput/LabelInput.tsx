4dbe60a3eb6652f6eb7ab9c77f5853dfa9c53d2e
Karol Wolski <karolwolski04@gmail.com>
2023-02-17 22:46:29 +0100

    feat: Show and hide password value in password field

--- src/components/labelInput/LabelInput.tsx
+++ src/components/labelInput/LabelInput.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import styles from './LabelInput.module.scss'
 
 interface ILabelInput {
@@ -24,23 +25,39 @@ const LabelInput = ({
   multiple = false,
   inputRef,
 }: ILabelInput) => {
+  const [showPassword, setShowPassword] = useState(false)
+  const toggle = () => setShowPassword(prevState => !prevState)
+  const inputTypeIfPassword = type === 'password' && showPassword ? 'text' : 'password'
   return (
     <>
       <label htmlFor={id} className={`form-label ${!isLabelVisible && 'visually-hidden'} `}>
         {labelText}
       </label>
-      <input
-        type={type}
-        className={`form-control ${styles.input}`}
-        id={id}
-        onChange={onChange}
-        onKeyDown={onKeyDown}
-        name={id}
-        placeholder={placeholder}
-        defaultValue={value}
-        multiple={multiple}
-        ref={inputRef}
-      />
+      <div className='position-relative'>
+        <input
+          type={type === 'password' ? inputTypeIfPassword : type}
+          className={`form-control ${styles.input}`}
+          id={id}
+          onChange={onChange}
+          onKeyDown={onKeyDown}
+          name={id}
+          placeholder={placeholder}
+          defaultValue={value}
+          multiple={multiple}
+          ref={inputRef}
+        />
+        {type === 'password' && (
+          <button
+            type='button'
+            onClick={toggle}
+            className='position-absolute top-0 end-0 h-100 bg-transparent border-0'
+            aria-label={showPassword ? 'Hide password' : 'Show password'}
+            title={showPassword ? 'Hide password' : 'Show password'}
+          >
+            {showPassword ? <i className='bi bi-eye-slash'></i> : <i className='bi bi-eye'></i>}
+          </button>
+        )}
+      </div>
     </>
   )
 }
