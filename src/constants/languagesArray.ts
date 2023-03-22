fc22eb4f1c1e621d745cdefab6f195354a9ed69a
Karol Wolski <karolwolski04@gmail.com>
2023-03-23 00:49:48 +0100

    feat: Add translation (PL/EN) to Settings (#184)

--- src/constants/languagesArray.ts
+++ src/constants/languagesArray.ts
@@ -1,13 +1,24 @@
-const LANGUAGE_ARRAY = [
+interface ILanguageObj {
+  id: number
+  value: string
+  name: string
+  translateId: string
+}
+
+export type languageArray = ILanguageObj[]
+
+const LANGUAGE_ARRAY: languageArray = [
   {
     id: 1,
     value: 'pl',
     name: 'Polish',
+    translateId: 'app.polish',
   },
   {
     id: 2,
     value: 'en',
     name: 'English',
+    translateId: 'app.english',
   },
 ]
 
