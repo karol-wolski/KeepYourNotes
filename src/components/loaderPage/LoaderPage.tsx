ff4760a7ca3f89fd407de951549523ac0cc9b82f
Karol Wolski <karolwolski04@gmail.com>
2023-04-07 23:37:35 +0200

    refactor(notes): Move no notes elements to separate component

--- src/components/loaderPage/LoaderPage.tsx
+++ src/components/loaderPage/LoaderPage.tsx
@@ -6,7 +6,11 @@ interface ILoaderPage {
 
 const LoaderPage = ({ isDark = true }: ILoaderPage) => {
   return (
-    <div className={`d-flex justify-content-center align-items-center vh-100 ${isDark && styles.loaderPage}`}>
+    <div
+      className={`d-flex justify-content-center align-items-center position-absolute top-0 left-0 vh-100 w-100 ${
+        isDark && styles.loaderPage
+      }`}
+    >
       <div className='spinner-border text-light' role='status'>
         <span className='visually-hidden'>Loading...</span>
       </div>
