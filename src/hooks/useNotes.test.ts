781bad9ec0e7d46fff0d6bffbaeee9153c995956
Karol Wolski <karolwolski04@gmail.com>
2023-04-20 21:42:44 +0200

    test: Add some tests to hooks

---
+++ src/hooks/useNotes.test.ts
@@ -0,0 +1,53 @@
+import { renderHook, act } from '@testing-library/react'
+import * as useFetchHook from './useFetch'
+import useNotes from './useNotes'
+import { Note } from '../components/notes/Notes'
+
+const mockDataObj = {
+  _id: '1',
+  title: 'Note',
+  desc: 'This is first note.',
+  pinIt: false,
+}
+
+const mockDataObj2 = {
+  _id: '2',
+  title: 'Note 2',
+  desc: 'This is another note.',
+  pinIt: false,
+}
+
+const mockValue = {
+  data: [mockDataObj],
+  errors: '',
+  isLoading: false,
+  refresh: jest.fn(),
+  fetchData: jest.fn(),
+  successMsg: '',
+  clearSuccessMsg: jest.fn(),
+  statusCode: undefined,
+}
+
+const useFetchSpy = jest.spyOn(useFetchHook, 'default')
+
+describe('useCategories', () => {
+  beforeEach(() => {
+    useFetchSpy.mockReturnValue(mockValue)
+  })
+
+  test('should render initial state', () => {
+    const { result } = renderHook(useNotes<Note[]>, { initialProps: undefined })
+    expect(result.current.notes).toStrictEqual([mockDataObj])
+    expect(result.current.errors).toBe('')
+    expect(result.current.isLoading).toBe(false)
+  })
+
+  test('should update categories', () => {
+    const { result } = renderHook(useNotes<Note[]>, { initialProps: undefined })
+    expect(result.current.notes).toStrictEqual([mockDataObj])
+    act(() => result.current.update([mockDataObj, mockDataObj2]))
+    expect(result.current.notes).toStrictEqual([mockDataObj, mockDataObj2])
+    expect(result.current.errors).toBe('')
+    expect(result.current.isLoading).toBe(false)
+  })
+})
