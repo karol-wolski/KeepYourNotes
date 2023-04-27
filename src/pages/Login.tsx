a632345e115a30b605b300c0e343256f70f2289a
Karol Wolski <karolwolski04@gmail.com>
2023-04-27 23:48:06 +0200

    test: Login Form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,11 +3,33 @@ import { Link } from 'react-router-dom'
 import LoginForm from '../components/loginForm/LoginForm'
 import LayoutForm from '../layout/LayoutForm/LayoutForm'
 import styles from './Login.module.scss'
+import useFetch from '../hooks/useFetch'
+import { useContext, useEffect } from 'react'
+import { addToLocalStorage } from '../helpers/localStorage'
+import { AuthContext, IAuthContext } from '../context/AuthContext'
+
+interface IResponse {
+  token: string
+}
 
 const LoginPage = () => {
+  const { setIsLoggedIn } = useContext(AuthContext) as IAuthContext
+  const { data, errors, isLoading, fetchData, statusCode } = useFetch<IResponse>()
+
+  useEffect(() => {
+    if (statusCode === 200 && data) {
+      addToLocalStorage('token', data.token)
+      setIsLoggedIn((state: boolean) => !state)
+    }
+  }, [statusCode, data])
+
+  const onSubmit = (email: string, password: string) => {
+    fetchData('auth/login', 'POST', { email: email, password: password })
+  }
+
   return (
     <LayoutForm title={<FormattedMessage id='app.login' defaultMessage='Login' />}>
-      <LoginForm />
+      <LoginForm onSubmit={onSubmit} errors={errors} isLoading={isLoading} statusCode={statusCode} />
 
       <div className='d-flex justify-content-between align-items-center mt-2'>
         <Link to='/remind-password' className={`${styles.link} ${styles['link__reset']}`}>
