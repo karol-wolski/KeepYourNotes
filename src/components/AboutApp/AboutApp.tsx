c2597b12a63461d40c41e548749d24219a786953
Karol Wolski <karolwolski04@gmail.com>
2023-09-26 14:07:23 +0200

    feat: about app section

---
+++ src/components/AboutApp/AboutApp.tsx
@@ -0,0 +1,14 @@
+interface Props {
+  title: string
+  desc: string | React.ReactElement
+}
+const AboutApp = ({ title, desc }: Props) => {
+  return (
+    <section className='about-app d-flex flex-column align-items-center px-4'>
+      <h2>{title}</h2>
+      <p className='fs-5 about-app__desc lh-base'>{desc}</p>
+    </section>
+  )
+}
+
+export default AboutApp
