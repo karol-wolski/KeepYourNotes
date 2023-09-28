402a57923f17f7a413f0f176a2e58c9c60c6d789
Karol Wolski <karolwolski04@gmail.com>
2023-09-28 17:01:29 +0200

    feat: footer (#260)

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='footer d-flex justify-content-center'>
+      <a href='https://github.com/karol-wolski/KeepYourNotes'>KeepYourNotes</a>
+    </footer>
+  )
+}
+
+export default Footer
