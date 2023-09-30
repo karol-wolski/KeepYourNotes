26088b7e6b31dd89308c3e0d3ff18ca88957ed76
Karol Wolski <karolwolski04@gmail.com>
2023-09-30 23:59:01 +0200

    feat: Reviews

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import reportWebVitals from './reportWebVitals'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap/dist/js/bootstrap'
 import App from './App'
 import './index.css'
 
